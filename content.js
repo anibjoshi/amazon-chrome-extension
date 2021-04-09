cf7a8dbec216d027ad552d1325d7bbab2aee4039
Anuradha Boche <anuradhaboche@gmail.com>
2021-04-08 23:00:48 -0400

    Product reviews are appending

--- content.js
+++ content.js
@@ -1,75 +1,54 @@
 console.log("Executing content.js")
 
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse)=>{
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
     console.log('current message', msg.text)
-    if (msg.text=='get-initial-data'){
-        var domInfo={
+    if (msg.text == 'get-initial-data') {
+        console.log('setting initial data')
+        // console.log('price',document.getElementById('priceblock_ourprice').innerText)
+        var domInfo = {
             productTitle: document.getElementById('productTitle').innerText,
-            productPrice: document.getElementById('priceblock_ourprice').innerText,
+            // productPrice: document.getElementById('priceblock_ourprice').innerText,
             productAllReviewsLink: document.getElementsByClassName('a-link-emphasis a-text-bold')[0].href,
             overallProductRating: document.querySelectorAll("[data-hook='average-star-rating']")[0].innerText
         }
-    } else if (msg.text=='get-review-data') {
-        console.log('setting domInfo')
-        console.log('current dom', document)
-        console.log('review1Text:',document.getElementsByClassName("a-size-base review-text review-text-content")[0])
-        console.log('review1Title:',document.getElementsByClassName("a-size-base a-link-normal review-title a-color-base review-title-content a-text-bold")[0])
-        var domInfo={
-            review1Title:document.getElementsByClassName("a-size-base a-link-normal review-title a-color-base review-title-content a-text-bold")[0].innerText,
-            review1Title:document.getElementsByClassName("a-size-base a-link-normal review-title a-color-base review-title-content a-text-bold")[0].innerText,
-            review1Date: document.querySelectorAll("[data-hook='review-date']")[0].innerText,
-            review1Rating: document.querySelectorAll("[data-hook='review-star-rating']")[0].innerText,
-            review1Text: document.getElementsByClassName("a-size-base review-text review-text-content")[0].innerText,
-
-            review2Title:document.getElementsByClassName("a-size-base a-link-normal review-title a-color-base review-title-content a-text-bold")[1].innerText,
-            review2Date: document.querySelectorAll("[data-hook='review-date']")[1].innerText,
-            review2Rating: document.querySelectorAll("[data-hook='review-star-rating']")[1].innerText,
-            review2Text: document.getElementsByClassName("a-size-base review-text review-text-content")[1].innerText,
-
-            review3Title:document.getElementsByClassName("a-size-base a-link-normal review-title a-color-base review-title-content a-text-bold")[2].innerText,
-            review3Date: document.querySelectorAll("[data-hook='review-date']")[2].innerText,
-            review3Rating: document.querySelectorAll("[data-hook='review-star-rating']")[2].innerText,
-            review3Text: document.getElementsByClassName("a-size-base review-text review-text-content")[2].innerText,
-            
-            review4Title:document.getElementsByClassName("a-size-base a-link-normal review-title a-color-base review-title-content a-text-bold")[3].innerText,
-            review4Date: document.querySelectorAll("[data-hook='review-date']")[3].innerText,
-            review4Rating: document.querySelectorAll("[data-hook='review-star-rating']")[3].innerText,
-            review4Text: document.getElementsByClassName("a-size-base review-text review-text-content")[3].innerText,
-
-            review5Title:document.getElementsByClassName("a-size-base a-link-normal review-title a-color-base review-title-content a-text-bold")[4].innerText,
-            review5Date: document.querySelectorAll("[data-hook='review-date']")[4].innerText,
-            review5Rating: document.querySelectorAll("[data-hook='review-star-rating']")[4].innerText,
-            review5Text: document.getElementsByClassName("a-size-base review-text review-text-content")[4].innerText,
-
-            review6Title:document.getElementsByClassName("a-size-base a-link-normal review-title a-color-base review-title-content a-text-bold")[5].innerText,
-            review6Date: document.querySelectorAll("[data-hook='review-date']")[5].innerText,
-            review6Rating: document.querySelectorAll("[data-hook='review-star-rating']")[5].innerText,
-            review6Text: document.getElementsByClassName("a-size-base review-text review-text-content")[5].innerText,
-
-            review7Title:document.getElementsByClassName("a-size-base a-link-normal review-title a-color-base review-title-content a-text-bold")[6].innerText,
-            review7Date: document.querySelectorAll("[data-hook='review-date']")[6].innerText,
-            review7Rating: document.querySelectorAll("[data-hook='review-star-rating']")[6].innerText,
-            review7Text: document.getElementsByClassName("a-size-base review-text review-text-content")[6].innerText,
-
-            review8Title:document.getElementsByClassName("a-size-base a-link-normal review-title a-color-base review-title-content a-text-bold")[7].innerText,
-            review8Date: document.querySelectorAll("[data-hook='review-date']")[7].innerText,
-            review8Rating: document.querySelectorAll("[data-hook='review-star-rating']")[7].innerText,
-            review8Text: document.getElementsByClassName("a-size-base review-text review-text-content")[7].innerText,
-
-            review9Title:document.getElementsByClassName("a-size-base a-link-normal review-title a-color-base review-title-content a-text-bold")[8].innerText,
-            review9Date: document.querySelectorAll("[data-hook='review-date']")[8].innerText,
-            review9Rating: document.querySelectorAll("[data-hook='review-star-rating']")[8].innerText,
-            review9Text: document.getElementsByClassName("a-size-base review-text review-text-content")[8].innerText,
-
-            review10Title:document.getElementsByClassName("a-size-base a-link-normal review-title a-color-base review-title-content a-text-bold")[9].innerText,
-            review10Date: document.querySelectorAll("[data-hook='review-date']")[9].innerText,
-            review10Rating: document.querySelectorAll("[data-hook='review-star-rating']")[9].innerText,
-            review10Text: document.getElementsByClassName("a-size-base review-text review-text-content")[9].innerText,
-
-            nextReviewsURL: document.getElementsByClassName("a-last")[0].getElementsByTagName('a')[0].href
-            } 
+    } else if (msg.text == 'get-review-data') {
+        console.log('setting domInfo');
+        reviews = {}
+        var i = 0;
+        var length = document.getElementsByClassName("a-size-base a-link-normal review-title a-color-base review-title-content a-text-bold").length
+        for (i = 0; i < length; i++) {
+            reviewTitle = document.getElementsByClassName("a-size-base a-link-normal review-title a-color-base review-title-content a-text-bold")[i].innerText
+            reviewDate = document.querySelectorAll("[data-hook='review-date']")[i].innerText
+            reviewRating = document.querySelectorAll("[data-hook='review-star-rating']")[i].innerText
+            reviewText = document.getElementsByClassName("a-size-base review-text review-text-content")[i].innerText
+            // new_review = {
+            //     'reviewTitle': reviewTitle,
+            //     'reviewDate': reviewDate,
+            //     'reviewRating': reviewRating,
+            //     'reviewText': reviewText
+            // }
+            new_review={}
+            new_review['reviewTitle'+i]=reviewTitle
+            new_review['reviewDate'+i]=reviewDate
+            new_review['reviewRating'+i]=reviewRating
+            new_review['reviewText'+i]=reviewText
+            reviews = Object.assign( reviews, new_review)
         }
-        console.log("sending domInfo!",domInfo)
-        sendResponse(domInfo)
-        return True
+        var next_url = document.getElementsByClassName("a-last")[0].getElementsByTagName('a')[0]
+        console.log('next_url',next_url)
+        if (typeof next_url !== 'undefined') {
+            next_url_dict = {
+                'nextReviewsURL': document.getElementsByClassName("a-last")[0].getElementsByTagName('a')[0].href
+            }
+        } else {
+            // next_url_dict = Add the main product link here
+            next_url_dict={
+                'baseURL':'https://www.amazon.com/MUDEELA-Planter-Watering-Drainage-Shortage/dp/B088FTL4F1/ref=cm_cr_arp_d_product_top?ie=UTF8'
+            }
+        }
+        domInfo = Object.assign( reviews, next_url_dict)
+    }
+    console.log("sending domInfo!", domInfo)
+    sendResponse(domInfo)
+    return True
 });
--- popup.js
+++ popup.js
@@ -1,41 +1,85 @@
 console.log("Executing popup.js")
 
-var getInitialData = info=>{
-    document.querySelector('#product-title').textContent= info.productTitle,
-    document.querySelector('#product-price').textContent= info.productPrice,
-    document.querySelector('#product-reviews-link').textContent= info.productAllReviewsLink
-    document.querySelector('#product-rating').textContent= info.overallProductRating
-    chrome.tabs.update({url : info.productAllReviewsLink})
-}
-
-var getReviewData=info=> {
-    console.log("info "+info)
-    console.log("infoTitle "+info.review1Title)
-    document.querySelector("#product-review1-title").textContent= info.review1Title,
-    document.querySelector("#product-review1-date").textContent= info.review1Date,
-    document.querySelector("#product-review1-rating").textContent= info.review1Rating,
-    document.querySelector("#product-review1-text").textContent= info.review1Text,
-
-    document.querySelector("#product-review2-title").textContent= info.review2Title,
-    document.querySelector("#product-review2-date").textContent= info.review2Date,
-    document.querySelector("#product-review2-rating").textContent= info.review2Rating,
-    document.querySelector("#product-review2-text").textContent= info.review2Text
-    
-    document.querySelector("#next-reviews-url").textContent= info.nextReviewsURL
-    chrome.tabs.update({url : info.nextReviewsURL})
-}
-document.addEventListener("DOMContentLoaded", ()=>{
-    console.log("Dom loaded and sending message to content.js")
-    document.getElementById("start-your-magic").addEventListener('click',function(){
-    chrome.tabs.query({active: true, currentWindow: true}, tabs=>{
-        chrome.tabs.sendMessage(tabs[0].id,  {text: 'get-initial-data'}, getInitialData);
+var getInitialData = info => {
+    console.log('Storing data in chrome storage')
+    product = {}
+    product_reviews = {}
+    product = JSON.stringify(info)
+    console.log("product:", product)
+    chrome.storage.local.set({ 'product_details': product }, function () {
+        console.log('Added product.json to chrome.storage!')
     })
+    chrome.storage.local.set({ 'product_review_details': product_reviews }, function () {
+        console.log('Added product_review.json to chrome.storage!')
+    })
+    chrome.tabs.update({ url: info.productAllReviewsLink })
+
+}
+
+var getReviewData = info => {
+    // console.log("info ", info)
+    if (typeof info.baseURL !== "undefined") {
+        chrome.storage.local.get(['product_details', 'product_review_details'], function (result) {
+            console.log('chrome.storage product details ', result.product_details)
+            console.log('chrome.storage product reviews details', result.product_review_details)
+            //     var url = 'data:application/json;base64,' + btoa(result.product_details);
+            //     chrome.downloads.download({
+            //         url: url,
+            //         filename: 'product_details.json'
+            //     });
+            //     var url = 'data:application/json;base64,' + btoa(result.product_reviews_details);
+            //     chrome.downloads.download({
+            //         url: url,
+            //         filename: 'product_reviews_details.json'
+            //     });
+            // });
+            chrome.storage.local.remove(["product_details","product_review_details"],function(){
+                var error = chrome.runtime.lastError;
+                   if (error) {
+                       console.error(error);
+                   }
+               })
+        });
+    }
+    else {
+        console.log('1')
+        all_product_reviews={}
+        new_reviews = JSON.stringify(info)
+        chrome.storage.local.get(['product_review_details'], function(result){
+            stored_reviews= result["product_review_details"]
+            console.log("stored_reviews",stored_reviews)
+            all_product_reviews= stored_reviews+new_reviews
+            console.log("all_product_reviews", all_product_reviews)
+            chrome.storage.local.set({ 'product_review_details': all_product_reviews }, function () {
+                console.log('added reviews to chrome.storage')
+                })
+        })
+        // console.log('new_reviews', new_reviews)
+        // chrome.storage.local.get(['product_review_details'], function (result) {
+        //     product_reviews = result.product_review_details
+        //     console.log("product_reviews", product_reviews)
+        //     var new_json = Object.assign(JSON.parse(product_reviews), JSON.parse(new_reviews))
+        //     console.log('new_json', new_json)
+
+        // })
+        console.log('2')
+        chrome.tabs.update({ url: info.nextReviewsURL })
+        console.log('3')
+    }
+}
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Dom loaded and sending message to content.js")
+    document.getElementById("start-your-magic").addEventListener('click', function () {
+        chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+            chrome.tabs.sendMessage(tabs[0].id, { text: 'get-initial-data' }, getInitialData);
+        })
     });
 });
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if(tab.status=='complete') {
-        chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-            chrome.tabs.sendMessage(tabs[0].id,  {text: 'get-review-data'}, getReviewData);
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (tab.status == 'complete') {
+        chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+            console.log('bubu')
+            chrome.tabs.sendMessage(tabs[0].id, { text: 'get-review-data' }, getReviewData);
         });
     }
 });
