b7a8e41b17d9fd7608d2cb0142a9d6ec27532881
Aniruddha Joshi <aniruddha.joshi@ibm.com>
2021-05-10 10:39:52 -0400

    Test Dynamo Access

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import jsonify, request
 from app import app
 from app.APIs.Prediction import Prediction
 import json
-from pymongo import MongoClient
+import boto3
 
 @app.route('/')
 def index():
@@ -12,31 +12,39 @@ def index():
 def predict_summary():	
 
 	app.logger.info('---SUMMARY GENERATION---')
-	prediction=Prediction()
-	review_json = json.dumps(json.loads(request.data))
-	app.logger.info(review_json)
-	positive_reviews, negative_reviews, all_reviews,number_of_positive_reviews, number_of_negative_reviews,total_number_of_reviews=prediction.readContent(review_json)
-	app.logger.info('Starting to generate summaries')	
 
-	positive_summary=prediction.generate_summary(positive_reviews)
-	app.logger.info('Generated positive summary')
+	print('reviewId: ',request.data)
 
-	negative_summary=prediction.generate_summary(negative_reviews)
-	app.logger.info('Generated negative summary')
+	client = boto3.resource("dynamodb")
+	table = client.Table("review_analysis")
 
-	app.logger.info('---KEYWORD EXTRACTION---')
-	keywords= prediction.get_keywords(all_reviews)
-	app.logger.info('Keywords extracted')
+	response = table.get_item(
+        Key={
+            'reviewId':request.data
+        }
+    )
+	print(response)
 
-	output_json= json.dumps({'positive_summary':positive_summary, 'negative_summary': negative_summary, 'keywords': keywords,
-	'number_of_positive_reviews':number_of_positive_reviews,'number_of_negative_reviews': number_of_negative_reviews,
-	'total_number_of_reviews':total_number_of_reviews})
+	# prediction=Prediction()
+	# review_json = json.dumps(json.loads(request.data))
+	# app.logger.info(review_json)
+	# positive_reviews, negative_reviews, all_reviews,number_of_positive_reviews, number_of_negative_reviews,total_number_of_reviews=prediction.readContent(review_json)
+	# app.logger.info('Starting to generate summaries')	
 
-	app.logger.info('Returned output json',output_json)
-	# client = MongoClient('localhost', 27017)
-	# db = client['amazon_reviews_data']
-	# collection_data = db['reviews_json']
-	# collection_data.insert_one(output_json)
-	# client.close()
+	# positive_summary=prediction.generate_summary(positive_reviews)
+	# app.logger.info('Generated positive summary')
 
-	return output_json
+	# negative_summary=prediction.generate_summary(negative_reviews)
+	# app.logger.info('Generated negative summary')
+
+	# app.logger.info('---KEYWORD EXTRACTION---')
+	# keywords= prediction.get_keywords(all_reviews)
+	# app.logger.info('Keywords extracted')
+
+	# output_json= json.dumps({'positive_summary':positive_summary, 'negative_summary': negative_summary, 'keywords': keywords,
+	# 'number_of_positive_reviews':number_of_positive_reviews,'number_of_negative_reviews': number_of_negative_reviews,
+	# 'total_number_of_reviews':total_number_of_reviews})
+
+	# app.logger.info('Returned output json',output_json)
+
+	return 'Success'
